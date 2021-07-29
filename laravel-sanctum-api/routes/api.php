f0f9fdced066c835ade61be05c347135e45c2a83
pavel99pop <pavel99pop@gmail.com>
2021-07-29 23:57:58 +0600

    Initial, product model and controller, api to create new product

---
+++ laravel-sanctum-api/routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Product;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/products', function(){
+    return Product::all();
+});
+
+Route::post('/products', function(){
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
